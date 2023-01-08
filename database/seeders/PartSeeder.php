cbde15ff3e2e66f124a0a19735b76e354f76be7a
priyamghosh1 <priyamghosh73@gmail.com>
2023-01-08 09:37:12 +0530

    part table created and 2 assembly seeding updated

--- database/seeders/PartSeeder.php
+++ database/seeders/PartSeeder.php
@@ -78,6 +78,62 @@ class PartSeeder extends Seeder
         Part::create(['assembly_id' => 1, 'part_name' => 'LAPANGAP B' , 'part_number'=> '61']);
 
         //assembly 2
-        
+        Part::create(['assembly_id' => 2, 'part_name' => 'MADURA' , 'part_number'=> '1']);
+        Part::create(['assembly_id' => 2, 'part_name' => 'MADUR B' , 'part_number'=> '2']);
+        Part::create(['assembly_id' => 2, 'part_name' => 'NIRIANG' , 'part_number'=> '3']);
+        Part::create(['assembly_id' => 2, 'part_name' => 'LADMUKHLA A.' , 'part_number'=> '4']);
+        Part::create(['assembly_id' => 2, 'part_name' => 'LADMUKHLA B' , 'part_number'=> '5']);
+        Part::create(['assembly_id' => 2, 'part_name' => 'UMLANGSHOR' , 'part_number'=> '6']);
+        Part::create(['assembly_id' => 2, 'part_name' => 'MUKHLA SHOHSHRIEH' , 'part_number'=> '7']);
+        Part::create(['assembly_id' => 2, 'part_name' => 'MUKHLA A (MALE)' , 'part_number'=> '8']);
+        Part::create(['assembly_id' => 2, 'part_name' => 'MUKHLA B (FEMALE)' , 'part_number'=> '9']);
+        Part::create(['assembly_id' => 2, 'part_name' => 'MUKHLA MISSION' , 'part_number'=> '10']);
+        Part::create(['assembly_id' => 2, 'part_name' => 'PHLONGINGKHAW' , 'part_number'=> '11']);
+        Part::create(['assembly_id' => 2, 'part_name' => 'MOOKYNIANG A (MALE)' , 'part_number'=> '12']);
+        Part::create(['assembly_id' => 2, 'part_name' => 'MOOKYNIANG B (FEMALE)' , 'part_number'=> '13']);
+        Part::create(['assembly_id' => 2, 'part_name' => 'MOOPYLLAITSYIAR' , 'part_number'=> '14']);
+        Part::create(['assembly_id' => 2, 'part_name' => 'KHLIEHTYRSHI' , 'part_number'=> '15']);
+        Part::create(['assembly_id' => 2, 'part_name' => 'MIHMYNTDU LUMPYRDI A (MALE)' , 'part_number'=> '16']);
+        Part::create(['assembly_id' => 2, 'part_name' => 'MIHMYNTDU LUMPYRDI B(FEMALE)' , 'part_number'=> '17']);
+        Part::create(['assembly_id' => 2, 'part_name' => 'MIHMYNTDU LUMPYRTUH A (MALE)' , 'part_number'=> '18']);
+        Part::create(['assembly_id' => 2, 'part_name' => 'MIHMYNTDU LUMPYRTUH B (FEMALE)' , 'part_number'=> '19']);
+        Part::create(['assembly_id' => 2, 'part_name' => 'MOOKYNPHAD MIHMYNTDU' , 'part_number'=> '20']);
+        Part::create(['assembly_id' => 2, 'part_name' => 'MIHMYNTDU LUMPYRTUH SALINI COLONY' , 'part_number'=> '21']);
+        Part::create(['assembly_id' => 2, 'part_name' => 'SABAHMUSWANG A (MALE)' , 'part_number'=> '22']);
+        Part::create(['assembly_id' => 2, 'part_name' => 'SABAHMUSWANG B(FEMALE)' , 'part_number'=> '23']);
+        Part::create(['assembly_id' => 2, 'part_name' => 'SOHMYNTING A (MALE)' , 'part_number'=> '24']);
+        Part::create(['assembly_id' => 2, 'part_name' => 'SOHMYNTING B (FEMALE)' , 'part_number'=> '25']);
+        Part::create(['assembly_id' => 2, 'part_name' => 'SOHMYNTING DONGWAH' , 'part_number'=> '26']);
+        Part::create(['assembly_id' => 2, 'part_name' => 'IALONG A (MALE)' , 'part_number'=> '27']);
+        Part::create(['assembly_id' => 2, 'part_name' => 'IALONG B (FEMALE)' , 'part_number'=> '28']);
+        Part::create(['assembly_id' => 2, 'part_name' => 'IALONG LUTI TUBER' , 'part_number'=> '29']);
+        Part::create(['assembly_id' => 2, 'part_name' => 'LADTHADLABOH A.' , 'part_number'=> '30']);
+        Part::create(['assembly_id' => 2, 'part_name' => 'CAROLINE COLONY' , 'part_number'=> '31']);
+        Part::create(['assembly_id' => 2, 'part_name' => 'LADTHADLABOH B' , 'part_number'=> '32']);
+        Part::create(['assembly_id' => 2, 'part_name' => 'LADTHADLABOH C' , 'part_number'=> '33']);
+        Part::create(['assembly_id' => 2, 'part_name' => 'MOOKYRDUP' , 'part_number'=> '34']);
+        Part::create(['assembly_id' => 2, 'part_name' => 'DONGMIHSNGI A.' , 'part_number'=> '35']);
+        Part::create(['assembly_id' => 2, 'part_name' => 'DONGMIHSNGI B' , 'part_number'=> '36']);
+        Part::create(['assembly_id' => 2, 'part_name' => 'IAWMUSIANG A (MALE)' , 'part_number'=> '37']);
+        Part::create(['assembly_id' => 2, 'part_name' => 'IAWMUSIANG B (FEMALE)' , 'part_number'=> '38']);
+        Part::create(['assembly_id' => 2, 'part_name' => 'MYNTHONG A' , 'part_number'=> '39']);
+        Part::create(['assembly_id' => 2, 'part_name' => 'MYNTHONG B' , 'part_number'=> '40']);
+        Part::create(['assembly_id' => 2, 'part_name' => 'CHUTWAKHU A (MALE)' , 'part_number'=> '41']);
+        Part::create(['assembly_id' => 2, 'part_name' => 'CHUTWAKHU B (FEMALE)' , 'part_number'=> '42']);
+        Part::create(['assembly_id' => 2, 'part_name' => 'KHIMMUSNIANG A (MALE)' , 'part_number'=> '43']);
+        Part::create(['assembly_id' => 2, 'part_name' => 'KHIMMUSNIANG B (FEMALE)' , 'part_number'=> '44']);
+        Part::create(['assembly_id' => 2, 'part_name' => 'MISSION COMPOUND A (MALE)' , 'part_number'=> '45']);
+        Part::create(['assembly_id' => 2, 'part_name' => 'MISSION COMPOUND B (FEMALE)' , 'part_number'=> '46']);
+        Part::create(['assembly_id' => 2, 'part_name' => 'CHILLIANGRAU A.' , 'part_number'=> '47']);
+        Part::create(['assembly_id' => 2, 'part_name' => 'CHILLIANGRAU B' , 'part_number'=> '48']);
+        Part::create(['assembly_id' => 2, 'part_name' => 'DULONG A (MALE)' , 'part_number'=> '49']);
+        Part::create(['assembly_id' => 2, 'part_name' => 'DULONG B (FEMALE)' , 'part_number'=> '50']);
+        Part::create(['assembly_id' => 2, 'part_name' => 'DULONG C' , 'part_number'=> '51']);
+        Part::create(['assembly_id' => 2, 'part_name' => 'DULONG D' , 'part_number'=> '52']);
+        Part::create(['assembly_id' => 2, 'part_name' => 'TPEPPALE' , 'part_number'=> '53']);
+        Part::create(['assembly_id' => 2, 'part_name' => 'PANALIAR A (MALE)' , 'part_number'=> '54']);
+        Part::create(['assembly_id' => 2, 'part_name' => 'PANALIAR B (FEMALE)' , 'part_number'=> '55']);
+        Part::create(['assembly_id' => 2, 'part_name' => 'IONGPIAH A (MALE)' , 'part_number'=> '56']);
+        Part::create(['assembly_id' => 2, 'part_name' => 'IONGPIAH B (FEMALE)' , 'part_number'=> '57']);
     }
 }
